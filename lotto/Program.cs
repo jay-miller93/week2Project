1b31844fb328351ba74ed185c5266da12f698ea9
Jay <isaacsdaddy42293@gmail.com>
2017-07-07 15:25:46 -0400

    first commit

---
+++ lotto/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lotto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //get user input
+            Console.WriteLine("Hello! and welcome to The Lottery Game!!");
+
+            Console.WriteLine("\n" + "Please select a low number for the first number in your number range:");
+            int lowNum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n" + "Great! Now, please select a high number for the end of your range(at least 50   more than low number):");
+            int highNum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n" + "Please enter six Lucky Numbers that are within the number range you have        selected:");
+            int[] luckyNum = new int[6];
+           
+        }        
+    }
+}
